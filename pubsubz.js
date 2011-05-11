26292aaceca360b46a70945adbeb9e18ac47fd60
addyosmani <addyosmani@gmail.com>
2011-05-11 06:25:18 +0100

    adding brief note on gpl

--- pubsubz.js
+++ pubsubz.js
@@ -1,48 +1,54 @@
-/*http://jsfiddle.net/vkVLk/10/*/
-
+/*
+	Copyright Addy Osmani 2011.
+	Released under the GPL
+*/
 var pubsubz = {};
-(function(q){
-    
+(function (q) {
+
     var topics = {},
         subUid = -1;
 
 
-    q.publish = function( topic , args ){
+    q.publish = function (topic, args) {
 
-       if(!topics[topic]){    
+        if (!topics[topic]) {
             return false;
         }
-        
-        setTimeout( function(){
-       
-			var subscribers = topics[topic],
-			len = subscribers ? subscribers.length :0; 
 
-        	while(len--){
-            	subscribers[len].func(topic, args);
-        	}}, 0);
+        setTimeout(function () {
+
+            var subscribers = topics[topic],
+                len = subscribers ? subscribers.length : 0;
+
+            while (len--) {
+                subscribers[len].func(topic, args);
+            }
+        }, 0);
 
         return true;
 
     };
-    
-    q.subscribe = function( topic, func ){
 
-       if(!topics[topic]){
+    q.subscribe = function (topic, func) {
+
+        if (!topics[topic]) {
             topics[topic] = [];
         }
 
         var token = (++subUid).toString();
-        topics[topic].push( { token : token, func : func } );
+        topics[topic].push({
+            token: token,
+            func: func
+        });
         return token;
     };
 
-    q.unsubscribe = function( token ){
-        for ( var m in topics ){
-            if ( topics[m] ){
-                for ( var i = 0, j = topics[m].length; i < j; i++ ){
-                    if ( topics[m][i].token === token ){
-                        topics[m].splice( i, 1 );
+    q.unsubscribe = function (token) {
+        for (var m in topics) {
+            if (topics[m]) {
+                for (var i = 0, j = topics[m].length; i < j; i++) {
+                    if (topics[m][i].token === token) {
+                        topics[m].splice(i, 1);
                         return token;
                     }
                 }
